b15d79731b59e00b01134f5abe990446ffd191b7
RodBC <rbc6@cin.ufpe.br>
2023-06-29 17:07:44 -0300

    criando as entidades dos usuários e dos grupos de iniciativas, junto com suas funcionalidades

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { GroupsModule } from './groups/groups.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [UsersModule, GroupsModule],
 })
 export class AppModule {}
