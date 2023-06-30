522612b9f86bb52575112b0359a777503b5d2453
RodBC <rbc6@cin.ufpe.br>
2023-06-30 18:03:07 -0300

    adicionando swagger initial doc

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,6 +1,23 @@
 
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CreateUserDto{
     id: string;
+    @ApiProperty(
+        {description: "the name of a given user"}
+    )
     name: string;
+    @ApiProperty(
+        {description: "the email of a given user"}
+    )
     email: string;
 }
+
+
+//example doc:
+// @ApiProperty({
+//     description: 'The age of a cat',
+//     minimum: 1,
+//     default: 1,
+//   })
+//   age: number;
