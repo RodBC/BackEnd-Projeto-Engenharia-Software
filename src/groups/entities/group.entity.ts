fd6d53afda9b3a9d895554c96e9eb8a6614ad3e4
RodBC <rbc6@cin.ufpe.br>
2023-07-02 19:49:12 -0300

    ajustando colunas das entidades

--- src/groups/entities/group.entity.ts
+++ src/groups/entities/group.entity.ts
@@ -1,1 +1,13 @@
-export class Group {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Group {
+    @Column() 
+    id: string;
+
+    @Column()
+    name: string;
+
+    @Column()
+    email: string;
+}
--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,5 +1,13 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
 export class User {
+    @Column() 
     id: string;
+
+    @Column()
     name: string;
+
+    @Column()
     email: string;
 }
